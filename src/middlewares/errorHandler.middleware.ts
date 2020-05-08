7c01b566b08bcffb88b115c7b5425dc14f929e12
Your Name <you@example.com>
2020-05-07 21:16:06 -0500

    Error handler

---
+++ src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express'
+
+export function handleError(err: any, req: Request, res: Response, next: NextFunction): void {
+  let payload: String
+  if (err && err.status && err.status != 500) {
+    payload = err.message
+    res.status(err.status)
+  } else {
+    payload = "Internal server error"
+    res.status(500)
+  }
+  res.end()
+}
